38ce8371aa0e487f3007f5fedc4c40555a8b8762
entradajuan <entradajuan@yahoo.es>
2017-08-06 12:37:36 +0200

    Cleansing traces, solving spaces problem

--- Analyzer/src/main/java/com/needine/spark/Prog1.scala
+++ Analyzer/src/main/java/com/needine/spark/Prog1.scala
@@ -4,7 +4,33 @@ import org.apache.spark.sql.SparkSession
 
 object Prog1 {
   case class Trace (ori: String, des: String, pro: String, bytes: Int)
-    
+   
+  def cleasingOri(cad: String): String = {
+    if (cad == null) {return "NADA"}
+    if (cad.split("→").length < 1) {return "NADA"}
+    if (cad.split("→")(0).split(" +").length < 1) {return "NADA"}
+    if (cad.split("→")(0).split(" +").last == "") {return "NADA"}
+    cad.split("→")(0).split(" +").last+";;"
+  }
+
+  def cleasingDest(cad: String): String = {
+    if (cad == null) {return "NADA1"}
+    if (cad.split("→").length < 2) {return "NADA2"}
+    if (cad.split("→")(1).split(" +").length < 2) {return "NADA3"}
+    if (cad.split("→")(1).split(" +").last == "") {return "NADA4"}
+    cad.split("→")(1).split(" +")(1)+";;"
+  }
+
+  
+  def cleasingProt(cad: String): String = {
+    if (cad == null) {return "NADA"}
+    if (cad.split("→").length < 2) {return "NADA"}
+    if (cad.split("→")(1).split(" +").length < 2) {return "NADA"}
+    if (cad.split("→")(1).split(" +")(2) == "") {return "NADA"}
+    cad.split("→")(1).split(" +")(2)+";;"
+  }
+  
+  
   def main(args: Array[String]) = {
 
     val spark = SparkSession
@@ -31,7 +57,10 @@ object Prog1 {
 
     
     //val packetsDS = lines.map{cad => Trace(cad._2.split("→")(0).split(" ").last, cad._2.split("→")(1).split(" ")(1), cad._2.split("→")(1).split(" ")(2), 5)}//.select($"ori")
-    val packetsDS = lines.map{cad => Trace("AAA", "BBB", cad._2.split("→")(1).split(" ")(2), 5)}//.select($"ori")
+    
+    
+    //val packetsDS = lines.map{cad => Trace(checkOri(cad._2.split("→")(0).split(" ").last), checkDest(cad._2.split("→")(1).split(" ")(1)), cad._2.split("→")(1).split(" ")(2),5/*, checkBytes(cad._2.split("→")(1).split(" ")(3))*/)}
+    val packetsDS = lines.map{cad => Trace(cleasingOri(cad._2), cleasingDest(cad._2), cleasingProt(cad._2),5/*, checkBytes(cad._2.split("→")(1).split(" ")(3))*/)}
     
     //packetsDS.printSchema()
     /*
@@ -44,7 +73,8 @@ object Prog1 {
     query.awaitTermination()
 		*/
     
-    val query = packetsDS.select($"ori" as "key", $"pro" as "value")
+    val query = packetsDS.toDF.withColumn("x", concat($"ori",  $"des", $"pro")/*+ ";" + $"des" + ";" +$"pro"*/ )
+      .select($"ori" as "key", $"x" as "value")
       .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
       .writeStream
       .format("kafka")
