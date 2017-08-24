1ea4d66950e0e0244f4b2e9f78df472c8ad4ca85
entradajuan <entradajuan@yahoo.es>
2017-08-24 18:26:18 +0200

    Improving Streaming writing queries to cassandra DB

--- Analyzer/src/main/java/com/needine/spark/ToCassandra2.scala
+++ Analyzer/src/main/java/com/needine/spark/ToCassandra2.scala
@@ -2,6 +2,7 @@ package com.needine.spark
 
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.ForeachWriter
 import com.datastax.spark.connector.cql.CassandraConnector
 import com.needine.spark.Tables.Origin_By_IP_TCP
 import com.needine.spark.Tables.Packet
@@ -23,34 +24,16 @@ object ToCassandra2 {
       }
     }
   }
-
-  
   
   def convert2Double(arr: Array[String]):Double={ 
-    //val arr = ip.split(".")
-    //println(ip)
-    //arr(0).concat(arr(1).concat(arr(2).concat(arr(3)))).toInt 
-    //arr(0).toInt
-    //"1231231231321".toDouble
     if(arr.length==4){
       arr(0).concat(arr(1).concat(arr(2).concat(arr(3)))).toDouble
     }else{
       0.0
     }
-    // 
-    
-    //arr.length
   }
     
-    
   def main(args: Array[String]) = {
-    /*
-    val spark = SparkSession
-      .builder()
-      .appName("StructuredStreamingDataToCassandra")
-      .config("spark.master", "local[*]")
-      .getOrCreate()
-   	*/
     
     val spark = SparkSession.builder
       .config("spark.master", "local[*]")
@@ -63,22 +46,14 @@ object ToCassandra2 {
     import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
     import org.apache.spark.sql.execution.debug._
     import org.apache.spark.sql.functions._
-    
 
+    spark.sparkContext.setLogLevel("WARN")
+        
     val conn = CassandraConnector.apply(spark.sparkContext.getConf)
-    val conn2 = CassandraConnector.apply(spark.sparkContext.getConf)
-
     conn.withSessionDo { session =>
       Statements.createKeySpace(session)
     }
-    conn2.withSessionDo { session =>
-      Statements.createKeySpace(session)
-    }
-
-    
     println("Cassandra KeySpace Initialized!!")
-
-    spark.sparkContext.setLogLevel("WARN")
        
     val df = spark
       .readStream
@@ -86,36 +61,10 @@ object ToCassandra2 {
       .option("kafka.bootstrap.servers", "localhost:9092,anotherhost:9092")
       .option("subscribe", "cleanedData")
       .load()
-      //.withWatermark(eventTime = "timestamp", delayThreshold = "10 seconds")
       
     val lines = df.selectExpr("CAST(timestamp AS TIMESTAMP)", "CAST(value AS STRING)")//.select($"timestamp", $"value").withColumn("unix_arrival", unix_timestamp($"timestamp")).withColumn("unix_time_now", unix_timestamp)
-    //lines.printSchema()
-
-    
-    
-    /*
-    val packets = lines
-      //.withWatermark("timestamp", "20 seconds")
-      .groupBy(window($"timestamp", "10 seconds"),$"value").count().withColumn("end", $"window.end")
-      .select("value", "count", "end" )
-      .selectExpr("CAST(value AS STRING)", "CAST(count AS Long)" ,"CAST(end AS TIMESTAMP)" ).withColumn("end2", unix_timestamp($"end"))
-      .filter($"end2".gt(unix_timestamp - 20))
-      .sort(desc("end"))
-    
-    packets.printSchema()
-    */
-
-    val cols = List("time", "bytes")
-    val df2 = lines.toDF(cols: _*)
-    //df2.printSchema()
     
     val originIP_TCP = lines.select($"value").as[String].map(_.split(";")).filter(arr=> arr(2)=="TCP").map(arr => (arr(0), convert2Double(arr(0).split("\\.")))).map{t=>
-        /*
-        if(t._1.split(".").length>0) {
-          Origin_By_IP_TCP(t._1.split(".")(0), t._2)          
-        }else{
-          Origin_By_IP_TCP("AAA", t._2)
-        }*/
         Origin_By_IP_TCP(t._1, t._2)
       }
     
@@ -123,35 +72,11 @@ object ToCassandra2 {
       .map(arr => (convert2Double(arr(0).split("\\.")), convert2Double(arr(1).split("\\.")), getBytes(arr)  ) )
       .map{t =>
         Packet(System.currentTimeMillis() / 1000L,t._1,t._2,t._3)
-      
       } 
-    //val originIP_TCP2 = lines.select($"value").as[String].map(_.split(";")).filter(arr=> arr(2)=="TCP").map(arr => (arr(0), convert2Int(arr(0).split("."))))
     
-    /*
-    val query = lines
-      .withWatermark("timestamp", "20 seconds")
-      .groupBy(window($"timestamp", "10 seconds"),$"value").count().withColumn("end", $"window.end")
-      .select("value", "count", "end" )
-      .selectExpr("CAST(value AS STRING)", "CAST(count AS Long)" ,"CAST(end AS TIMESTAMP)" ).withColumn("end2", unix_timestamp($"end"))
-      .filter($"end2".gt(unix_timestamp - 20))
-      .sort(desc("end"))//.limit(5)
-      .writeStream
-//      .trigger(Trigger.ProcessingTime(10.seconds))
-      .outputMode("complete")
-      //.outputMode("append")
-      .format("console")
-      .start()
-      
-    query.awaitTermination()
-    */
     
-    //val ds = df2/*.select( $"time", $"bytes")*/.as[Tables.Packet]
-    //ds.printSchema()
-    
+
     // This Foreach sink writer writes the output to cassandra.
-    import org.apache.spark.sql.ForeachWriter
-
-
     val writer2 = new ForeachWriter[Tables.Origin_By_IP_TCP] {
       override def open(partitionId: Long, version: Long) = true
       override def process(value: Tables.Origin_By_IP_TCP) = {
@@ -163,13 +88,13 @@ object ToCassandra2 {
     }
 
     val query2 = originIP_TCP.writeStream.queryName("StructuredStreamingDataToCassandra2").foreach(writer2).start
-    query2.awaitTermination()
-
+    //query2.awaitTermination()
     
+
     val writer = new ForeachWriter[Tables.Packet] {
       override def open(partitionId: Long, version: Long) = true
       override def process(value: Tables.Packet) = {
-        conn2.withSessionDo { session =>
+        conn.withSessionDo { session =>
           session.execute(Statements.savePacket(value.time, value.origen, value.destiny, value.bytes))
         }
       }
@@ -177,14 +102,24 @@ object ToCassandra2 {
     }
 
     val query = packet_TCP.writeStream.queryName("StructuredStreamingDataToCassandra3").foreach(writer).start
-    query.awaitTermination()
+
+    //query.awaitTermination()
+ 
+    // CHECK HOW TO CREATE a Streaming DS/DF
+    /*     
+    val df3 = spark.read.format("org.apache.spark.sql.cassandra").options(Map( "table" -> "origin_by_ip_tcp", "keyspace" -> "network_monitor" )).load()
+    df3.show(false)
+    df3.printSchema()
     
-    
+    val query3 = df3.select($"ip").writeStream
+    .outputMode("append")
+    .format("console")
+    .start()
+    query3.awaitTermination()
+    */
+
     spark.stop()
 
-    
-    
-    
     //Needine.com
     //Network Traffic Status V0.2
   }
